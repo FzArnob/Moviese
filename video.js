e24347afbb3c8d4b0f7abe300a6e27cc47364bbc
Farhan Zaman <farhanzamanarnob@gmail.com>
2022-01-23 05:30:48 +0600

    2

--- video.js
+++ video.js
@@ -43,5 +43,27 @@ getVideoData('`+dataEng[index].link+`');
 
 
 
-let datata = '{"query":"callback","type":"movie","title":"Ray Donovan (2022)","link":"`+dataEng[index].link+`","file":"https://www.mojaloss.stream/directlink/FTP7/English%20Movies/Ray%20Donovan%20(2022)/Ray.Donovan.The.Movie.2022.1080p.WEBRip.mp4","poster":"https://www.mojaloss.stream/wp-content/uploads/2022/01/MV5BMThkMTBiMDItZGVhOC00MWJkLThlZjgtNmJiMTA0NjFjMDgyXkEyXkFqcGdeQXVyOTA3MTMyOTk@.jpeg","track":"https://www.mojaloss.stream/directlink/FTP7/English%20Movies/Ray%20Donovan%20(2022)/Ray.Donovan.The.Movie.2022.1080p.WEBRip.srt"}'
+let datata = `{"query":"callback","type":"movie","title":"Ray Donovan (2022)","link":"","file":"https://www.mojaloss.stream/directlink/FTP7/English%20Movies/Ray%20Donovan%20(2022)/Ray.Donovan.The.Movie.2022.1080p.WEBRip.mp4","poster":"https://www.mojaloss.stream/wp-content/uploads/2022/01/MV5BMThkMTBiMDItZGVhOC00MWJkLThlZjgtNmJiMTA0NjFjMDgyXkEyXkFqcGdeQXVyOTA3MTMyOTk@.jpeg","track":"https://www.mojaloss.stream/directlink/FTP7/English%20Movies/Ray%20Donovan%20(2022)/Ray.Donovan.The.Movie.2022.1080p.WEBRip.srt"}`;
 loadVideo(datata);
+
+
+
+
+      function setCookie(cName, cValue, expDays) {
+            let date = new Date();
+            date.setTime(date.getTime() + (expDays * 24 * 60 * 60 * 1000));
+            const expires = "expires=" + date.toUTCString();
+            document.cookie = cName + "=" + cValue + "; " + expires + "; path=/";
+        }
+
+        function listCookies() {
+            var theCookies = document.cookie.split(';');
+            var aString = '\n';
+            for (var i = 1; i <= theCookies.length; i++) {
+                aString += i + ' ' + theCookies[i - 1] + "\n";
+            }
+            return aString;
+        }
+        // Apply setCookie
+        setCookie('wordpress_logged_in_68c526a5b224f2027adee3c7a7977de6', 'wordpress_logged_in_68c526a5b224f2027adee3c7a7977de6', 30);
+        console.log('cookies:', listCookies())
